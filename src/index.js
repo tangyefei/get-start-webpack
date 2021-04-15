260ab8aeb4a45f196db06e7713de466aa3a2ba30
tang.tyf <tang.tyf@alibaba-inc.com>
2021-04-15 11:36:08 +0800

    feat: 安装react并配置所需要的规则

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import hello from './hello';
-// import "@babel/polyfill";
-let set = new Set();
-set.add(1);
-set.add(2);
-set.add(3);
-document.write(hello());
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Search extends React.Component {
+  render() {
+    return <div>Search Component</div>
+  }
+}
+
+ReactDOM.render(
+  <Search/>,
+  document.getElementById('root')
+)
