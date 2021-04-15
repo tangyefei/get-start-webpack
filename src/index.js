3eef4d09a21255d7c75b118e37a25c85c0e69d35
tang.tyf <tang.tyf@alibaba-inc.com>
2021-04-15 16:27:34 +0800

    feat: 安装file-loader并处理图片

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
+import testImg from './testImg.jpg'
 class App extends React.Component {
   render() {
-    return <div className="app-container"><h3>Search Component</h3></div>
+    return <div className="app-container">
+      <img src={testImg}></img>
+      <h3>Search Component</h3>
+    </div>
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,8 @@ module.exports = {
       test: /\.css$/, use: ['style-loader','css-loader']
     }, {
       test: /\.less$/, use: ['style-loader','css-loader', 'less-loader']
+    }, {
+      test: /\.(png|jpe?g|gif)$/i, loader: 'file-loader'
     }]
   }
 }
